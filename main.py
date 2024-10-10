178b3e43ea2a9172cf8a5c598ef15466809d73a4
M L <martinn.lindbladd@gmail.com>
2024-10-10 12:38:03 +0200

    main gui interface decentrilazation

--- main.py
+++ main.py
@@ -1,32 +1,5 @@
-from saver import Saver
-import random
-from datetime import datetime
-
-class text:
-    #console text decoration codes
-    BGWHITE = '\033[107m'
-    BGCYAN = '\033[106m'
-    BGPURPLE = '\033[105m'
-    BGBLUE = '\033[104m'
-    BGYELLOW = '\033[103m'
-    BGGREEN = '\033[102m'
-    BGRED = '\033[101m'
-    BGGRAY = '\033[100m'
-    BGBLACK = '\033[40m'
-    
-    CYAN = '\033[96m'
-    PURPLE = '\033[95m'
-    BLUE = '\033[94m'
-    YELLOW = '\033[93m'
-    GREEN = '\033[92m'
-    RED = '\033[91m'
-    GRAY = '\033[90m'
-
-    STRIKETHROUGH = '\033[28m'
-    UNDERLINE = '\033[4m'
-    ITALICS = '\033[3m'
-    BOLD = '\033[1m'
-    END = '\033[0m'
+from generator import GroupGenerator
+from textdecoration import textdecoration as text
 
 class Command:
     def __init__(self, func, command: list, desc: str, help: str) -> None:
@@ -34,88 +7,6 @@ class Command:
         self.command = command
         self.desc = desc
         self.help = help
-
-class GroupGenerator:
-    def __init__(self) -> None:
-        self.saver = Saver()
-
-        self.entries = []
-        self.group_size = 1
-        self.groups = [],[]
-
-    def add(self, args = []) -> str:
-        retur = ""
-        for name in args:
-            self.entries.append(name)
-            retur+=f"{name} "
-
-        return retur
-        
-
-    def remove(self, args = []) -> str:
-        retur = ""
-        returNO = ""
-        if args[0] in ['all']:
-                self.entries = []
-                return 'All'
-
-        for name in args:
-            if name in self.entries:
-                self.entries.remove(name)
-                retur+=f"{name} "
-            else:
-                returNO+=f"{name} "
-
-        return retur, returNO
-
-    def sizeset(self, args = 2) -> int:
-        try:
-            self.group_size = abs(int(args[0]))
-            return self.group_size
-        except:
-            return None
-        
-
-    def gengroup(self, args = None) -> tuple:
-        groups = []
-        entries = self.entries.copy()
-        
-        while len(entries)>=self.group_size or self.group_size == 0:
-            group = random.sample(entries, self.group_size)
-            groups.append(group)
-            [entries.remove(entry) for entry in group]
-        
-        if not len(groups)>0:
-            return None, entries
-
-        self.groups = groups.copy(), entries.copy()
-        self.saver.save(f"result {datetime.now().strftime('%Y-%m-%d [%H-%M-%S]')}.json", {"groups": groups, "Rest Group": entries})
-        return groups, entries
-    
-    def qlist(self, args = None) -> tuple:
-        return self.entries, self.group_size, self.groups
-
-    def save(self, args = datetime.now().strftime('%Y-%m-%d [%H-%M-%S]')) -> str:
-        if args:
-            name = f"{args}.json"
-            content = {"size": self.group_size, "entries": self.entries}
-            self.saver.save(name, content)
-            return name
-        else:
-            return None
-
-    def load(self, args = None) -> str:
-        if args:
-            name = f"{args}.json"
-            data = self.saver.load(name)
-            if data:
-                self.entries = data['entries']
-                self.group_size = data['size']
-            else:
-                return name, (None, None)
-            return name, (self.entries, self.group_size)
-        else:
-            return None, (None, None)
 
 class gui:
     def __init__(self, groupgenerator: GroupGenerator) -> None:
@@ -125,9 +16,9 @@ class gui:
         main_loop_active = True
         while (main_loop_active):
             
-            for command in self.commands:
-                #print(f"{text.CYAN}[{command[self.KEY_COMMAND][0]}] {text.BLUE}{command[self.KEY_DESC]}{text.END}")
+            for command in self.commands[1:]:
                 print(f"{text.CYAN}[{self.commands.index(command)}] {text.BLUE}{command.desc}{text.END}")
+            print(f"{text.CYAN}[{self.commands.index(self.commands[0])}] {text.BLUE}{self.commands[0].desc}{text.END}")
 
             qinput = input(f"{text.YELLOW}> "); print(text.END)
             qinput = qinput.split(' ')
@@ -137,24 +28,25 @@ class gui:
                     command.func(self, qinput[1:])
 
     def add(self, args = None):
-        print(f"{text.CYAN}Add:{text.END}")
         if not args:
-            names_in = [input(f"{text.YELLOW}Enter the name you want to add list:\n> ")]; print(text.END)
+            names_in: str = [input(f"{text.YELLOW}Enter names you want to add:\n> ")]; print(text.END)
         else:
             names_in = args
         
+        print(f"{text.CYAN}Add:{text.END}")
         names_out = self.gg.add(names_in)
-        for name in names_out.split(' '):
-            print(f"\t{text.BLUE}{name}{text.END}") 
+        print(f"\t{text.BLUE}{names_out}{text.END}") 
+        #for name in names_out.split(' '):
+        #    print(f"\t{text.BLUE}{name}{text.END}") 
         
 
     def remove(self, args = None):
-        print(f"{text.CYAN}Remove:{text.END}")
         if not args:
-            names_in = input(f"{text.YELLOW} Enter the name you want to remove:\n> "); print(text.END)
+            names_in = input(f"{text.YELLOW} Enter names you want to remove:\n> "); print(text.END)
         else:
             names_in = args
 
+        print(f"{text.CYAN}Remove:{text.END}")
         names_out, namesNo_out = self.gg.remove(names_in)
         for name in names_out.split(' '):
             print(f"\t{text.BLUE}{name}{text.END}")
@@ -165,14 +57,14 @@ class gui:
                 print(f"\t{text.RED}{name}{text.END}")
 
     def sizeset(self, args = None):
-        print(f"{text.CYAN}Size:{text.END}")
         if not args:
-            group_size_in = input(f"{text.YELLOW}Enter the group size you want to create:\n> "); print(text.END)    
+            group_size_in = input(f"{text.YELLOW}Enter group size of generated groups:\n> "); print(text.END)    
         else:    
             group_size_in = args[0]
 
         group_size_out = self.gg.sizeset(group_size_in)
         if group_size_out:
+            print(f"{text.CYAN}Size:{text.END}")
             print(f"\t{text.BLUE}{group_size_out}{text.END}")
         else:
             print(f"{text.RED}Invalid Input: {group_size_in}{text.END}")
@@ -183,7 +75,7 @@ class gui:
         
         if groups:
             for group in groups:
-                print(f"\tGroup {groups.index(group)}:{text.BLUE}{group}{text.END}")
+                print(f"\tGroup {groups.index(group)}: {text.BLUE}{group}{text.END}")
         else:
             print(f"No groups generated")
         
@@ -192,30 +84,48 @@ class gui:
     def qlist(self, args = None):
         entries, group_size, groups = self.gg.qlist()
         print(f"{text.CYAN}List:{text.END}")
-        print(f"\t{text.BLUE}{entries}{text.END}")
-        print(f"\t{text.BLUE}{group_size}{text.END}")
+        print(f"\t{text.BLUE}Persons: {", ".join(entries)}{text.END}")
+        print(f"\t{text.BLUE}Group Size: {group_size}{text.END}")
+        print(f"\t{text.BLUE}Latest Groups:{" ".join([f"\n\t\tGroup {groups[0].index(group)}: {", ".join(group)}" for group in groups[0]])}\n\t\tRest Group: {", ".join(groups[1])}{text.END}")
+        print()
 
     def qexit(self, args = None):
         print(f"{text.PURPLE}Exit:{text.END}")
         exit(0)
 
-    def save(self, args = None):
-        qinput = None
+    def filenamefiletype(self, args):
+        filename = None
+        filetype = None
         if not args:
-            qinput = f"{input("Set File Name\n> ")}"
+            filename = f"{input("Set File Name\n> ")}"
+            filetype = f"{input("Set File Type\n> ")}"
         else:
-            qinput = f"{args[0]}"
-        
-        self.gg.save(qinput)
+            try: filetype = args[1] 
+            except: filetype = None
+            try: 
+                filename = args[0]
+                if len(filename.split('.'))>1:
+                    filetype = filename.split('.')[1]
+                    filename = filename.split('.')[0]
+            except: filename = None
+
+        return filename, filetype
+
+    def save(self, args = None):
+        filename, filetype = self.filenamefiletype(args)
+        if self.gg.save(filename, filetype):
+            print(f"Save {filename}.{filetype}")
+        else:
+            print(f"Failed: Save {filename}.{filetype}")
 
     def load(self, args = None):
-        qinput = None
-        if not args:
-            qinput = f"{input("Get File Name\n> ")}"
-        else:
-            qinput = f"{args[0]}"
+        filename, filetype = self.filenamefiletype(args)
 
-        self.gg.load(qinput)
+        if self.gg.load(filename, filetype):
+            print(f"Load {filename}.{filetype}")
+            self.qlist()
+        else:
+            print(f"Failed: Load {filename}.{filetype}")
 
     def help(self, args = None):
         for command in self.commands:
@@ -223,6 +133,7 @@ class gui:
         print()
     
     commands = [
+        Command(qexit, ['exit','x','e'], "Exit", "Exit Application"),
         Command(qlist, ['list','l', '='], "List", "Lists List and Size"),
         Command(add, ['add','+','a'], "Add Person", "Add Person to List [PERSON, PERSON, PERSON...]"),
         Command(remove, ['remove','-','r'], "Remove Person", "Remove Person from List [PERSON, PERSON, PERSON...]"),
@@ -230,7 +141,6 @@ class gui:
         Command(gengroup, ['gen','*','g'], "Generate Group List", "Generate groups by List and Size"),
         Command(save, ['save'], "Save instance", "Save current List and Size to File [FILENAME]"),
         Command(load, ['load'], "Load instance", "Load List and Size from File [FILENAME]"),
-        Command(qexit, ['exit','x','e'], "Exit", "Exit Application"),
         Command(help, ['help','?','h'], "Help", "Display Commands"),
     ]
 
